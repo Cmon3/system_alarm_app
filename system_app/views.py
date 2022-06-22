8ea3ca03bd039a828a46b5af3b50e3922684a4c4
Cmon3 <cmonlg@gmail.com>
2022-06-22 14:24:28 +0200

    second commit

--- system_app/views.py
+++ system_app/views.py
@@ -4,20 +4,20 @@ import json
 import random
 import time
 
-
+# Aqui guardo la data de los json en variables
 devices_file = apps.get_app_config(
     'system_app').path + '/json-data/devices.json'
 numbers_file = apps.get_app_config(
     'system_app').path + '/json-data/telephon_numbers.json'
 
+# funcion para activar el render de la página
+
 
 def alarm_status(request):
-    while(True):
-        time.sleep(3)
-        with open(devices_file) as devices_data:
-            devices = json.load(devices_data)
-        with open(numbers_file) as numbers_data:
-            phone_numbers = json.load(numbers_data)
-        rand_num = random.randrange(0, 6)
-        dataJson = json.dumps(devices)
-        return render(request, 'systemalerts.html', {'devices': devices, 'number': phone_numbers[rand_num], 'data': dataJson})
+    with open(devices_file) as devices_data:
+        devices = json.load(devices_data)
+    with open(numbers_file) as numbers_data:
+        phone_numbers = json.load(numbers_data)
+    rand_num = random.randrange(0, 6)
+    dataJson = json.dumps(devices)
+    return render(request, 'systemalerts.html', {'devices': devices, 'number': phone_numbers[rand_num], 'data': dataJson})
